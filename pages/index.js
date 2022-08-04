9e1f375a479df1a0ce240dd1729e8a7f753b7324
celestedeschamphelaere@github.com <celestedeschamphelaere@gmail.com>
2022-08-04 16:50:41 +0200

    about me and carousel

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,38 @@
-import React from 'react'
-import Waves from '../components/Waves'
+import React, { createContext } from 'react'
+import Wave from 'react-wavify'
 import Navbar from '../components/Navbar'
+import ProjectsCarousel from '../components/ProjectsCarousel'
+import Title from '../components/Title'
 
-export default function index() {
+export default function index({data}) {
   return (
     <>
-      <div className='min-h-[100vh] h-full text-simvoni bg-gray-300'>
+      <div className='min-h-[100vh] h-full text-simvoni bg-gray-200'>
         <div className='bg-orange-600 text-white flex flex-col'>
           <Navbar />
           <h1 className='pt-20 font-bold text-6xl self-center'>
             Celeste
           </h1>
         </div>
-        <Waves color="#EA580C" />
-        <div>
-          celeste
-        </div>
-        <div>
-          index
+        <Wave fill='#EA580C'
+          className='rotate-180'
+          paused={false}
+          options={{
+            height: 40,
+            amplitude: 40,
+            speed: 0.25,
+            points: 2
+          }} />
+        <div className='flex flex-col justify-center'>
+          <div className='self-center w-96 flex flex-col'>
+            <Title text="About me" />
+            <div className=''>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer mauris ligula, pellentesque at urna sit amet, pretium bibendum erat. Nulla pharetra tincidunt libero, in tincidunt enim. Vivamus eu justo sit amet est feugiat placerat. Mauris ante justo, ornare ut iaculis ac, vestibulum at metus. Vestibulum eu auctor justo.
+            </div>
+          </div>
+          <div className='self-center'>
+            <ProjectsCarousel projects={data} />
+          </div>
         </div>
       </div>
 
