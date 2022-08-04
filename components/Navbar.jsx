27b81e5a01bed3f1140145195e66770932b40e52
celestedeschamphelaere@github.com <celestedeschamphelaere@gmail.com>
2022-08-04 16:15:17 +0200

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='flex flex-row gap-10 mt-6 mr-10 text-xl font-semibold justify-end'>
+        <div>
+            About me
+        </div>
+        <div>
+            Projects
+        </div>
+        <div>
+            Experience
+        </div>
+        <div>
+            Contact
+        </div>
+    </div>
+  )
+}
